4e001156c59963e6924cc70123f83b17ad1755d6
AyalaTrachtman <ayala@gmail.com>
2026-04-30 01:04:01 +0300

    ex2

--- ex2.jl
+++ ex2.jl
@@ -1,339 +1,342 @@
-#Adi Toubin , id: 327915484  
-#Aylala Trachtman , id: 327869764
-# Arithmetic & Logical Handlers
-# Handles 'add' command: pop two values, add them, and push the result
+# Adi Toubin, id: 327915484
+# Aylala Trachtman, id: 327869764
+
+# --- Arithmetic & Logical Handlers ---
+
 function handleAdd()
-    return """
-    // add
-    @SP
-    AM=M-1
-    D=M
-    A=A-1
-    M=D+M
-    """
+    return """// add
+@SP
+AM=M-1
+D=M
+A=A-1
+M=D+M
+"""
 end
 
-# Handles 'sub' command: pop two values, subtract (y from x), and push the result
 function handleSub()
-    return """
-    // sub
-    @SP
-    AM=M-1
-    D=M
-    A=A-1
-    M=M-D
-    """
+    return """// sub
+@SP
+AM=M-1
+D=M
+A=A-1
+M=M-D
+"""
 end
 
-# Handles 'neg' command: negate the value at the top of the stack
 function handleNeg()
-    return """
-    // neg
-    @SP
-    A=M-1
-    M=-M
-    """
+    return """// neg
+@SP
+A=M-1
+M=-M
+"""
 end
 
-# Handles 'and' command: bitwise AND of the two top stack values
 function handleAnd()
-    return """
-    // and
-    @SP
-    AM=M-1
-    D=M
-    A=A-1
-    M=D&M
-    """
+    return """// and
+@SP
+AM=M-1
+D=M
+A=A-1
+M=D&M
+"""
 end
 
-# Handles 'or' command: bitwise OR of the two top stack values
 function handleOr()
-    return """
-    // or
-    @SP
-    AM=M-1
-    D=M
-    A=A-1
-    M=D|M
-    """
+    return """// or
+@SP
+AM=M-1
+D=M
+A=A-1
+M=D|M
+"""
 end
 
-# Handles 'not' command: bitwise NOT of the value at the top of the stack
 function handleNot()
-    return """
-    // not
-    @SP
-    A=M-1
-    M=!M
-    """
+    return """// not
+@SP
+A=M-1
+M=!M
+"""
 end
 
-# Comparison Handlers (with Labels) 
-
-# Handles 'eq' command: checks if two top values are equal
 function handleEq(counter)
     label = "EQ_$(counter)"
-    return """
-    // eq
-    @SP
-    AM=M-1
-    D=M
-    A=A-1
-    D=M-D
-    @$label.TRUE
-    D;JEQ
-    @SP
-    A=M-1
-    M=0
-    @$label.END
-    0;JMP
-    ($label.TRUE)
-    @SP
-    A=M-1
-    M=-1
-    ($label.END)
-    """
+    return """// eq
+@SP
+AM=M-1
+D=M
+A=A-1
+D=M-D
+@$label.TRUE
+D;JEQ
+@SP
+A=M-1
+M=0
+@$label.END
+0;JMP
+($label.TRUE)
+@SP
+A=M-1
+M=-1
+($label.END)
+"""
 end
 
-# Handles 'gt' command: checks if x > y
 function handleGt(counter)
     label = "GT_$(counter)"
-    return """
-    // gt
-    @SP
-    AM=M-1
-    D=M
-    A=A-1
-    D=M-D
-    @$label.TRUE
-    D;JGT
-    @SP
-    A=M-1
-    M=0
-    @$label.END
-    0;JMP
-    ($label.TRUE)
-    @SP
-    A=M-1
-    M=-1
-    ($label.END)
-    """
+    return """// gt
+@SP
+AM=M-1
+D=M
+A=A-1
+D=M-D
+@$label.TRUE
+D;JGT
+@SP
+A=M-1
+M=0
+@$label.END
+0;JMP
+($label.TRUE)
+@SP
+A=M-1
+M=-1
+($label.END)
+"""
 end
 
-# Handles 'lt' command: checks if x < y
 function handleLt(counter)
     label = "LT_$(counter)"
-    return """
-    // lt
-    @SP
-    AM=M-1
-    D=M
-    A=A-1
-    D=M-D
-    @$label.TRUE
-    D;JLT
-    @SP
-    A=M-1
-    M=0
-    @$label.END
-    0;JMP
-    ($label.TRUE)
-    @SP
-    A=M-1
-    M=-1
-    ($label.END)
-    """
+    return """// lt
+@SP
+AM=M-1
+D=M
+A=A-1
+D=M-D
+@$label.TRUE
+D;JLT
+@SP
+A=M-1
+M=0
+@$label.END
+0;JMP
+($label.TRUE)
+@SP
+A=M-1
+M=-1
+($label.END)
+"""
 end
 
-# Memory Access Handlers
+# --- Memory Access Handlers ---
 
-# Handles 'push' commands for various memory segments
 function handlePush(segment::String, index::Int, filename::String)
     header = "// push $segment $index"
     body = ""
     if segment == "constant"
-        body = """
-        @$index
-        D=A
-        @SP
-        A=M
-        M=D
-        @SP
-        M=M+1
-        """
+        body = "@$index\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
     elseif segment in ["local", "argument", "this", "that"]
         seg_map = Dict("local"=>"LCL", "argument"=>"ARG", "this"=>"THIS", "that"=>"THAT")
         label = seg_map[segment]
-        body = """
-        @$index
-        D=A
-        @$label
-        A=M+D
-        D=M
-        @SP
-        A=M
-        M=D
-        @SP
-        M=M+1
-        """
+        body = "@$index\nD=A\n@$label\nA=M+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
     elseif segment == "temp"
-        body = """
-        @$(5 + index)
-        D=M
-        @SP
-        A=M
-        M=D
-        @SP
-        M=M+1
-        """
+        body = "@$(5 + index)\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
     elseif segment == "pointer"
         ptr_label = index == 0 ? "THIS" : "THAT"
-        body = """
-        @$ptr_label
-        D=M
-        @SP
-        A=M
-        M=D
-        @SP
-        M=M+1
-        """
+        body = "@$ptr_label\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
     elseif segment == "static"
-        body = """
-        @$filename.$index
-        D=M
-        @SP
-        A=M
-        M=D
-        @SP
-        M=M+1
-        """
+        body = "@$filename.$index\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
     end
     return header * "\n" * body
 end
 
-# Handles 'pop' commands for various memory segments
 function handlePop(segment::String, index::Int, filename::String)
     header = "// pop $segment $index"
     body = ""
     if segment in ["local", "argument", "this", "that"]
         seg_map = Dict("local"=>"LCL", "argument"=>"ARG", "this"=>"THIS", "that"=>"THAT")
         label = seg_map[segment]
-        body = """
-        @$index
-        D=A
-        @$label
-        D=M+D
-        @R13
-        M=D
-        @SP
-        AM=M-1
-        D=M
-        @R13
-        A=M
-        M=D
-        """
+        body = "@$index\nD=A\n@$label\nD=M+D\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n"
     elseif segment == "temp"
-        body = """
-        @SP
-        AM=M-1
-        D=M
-        @$(5 + index)
-        M=D
-        """
+        body = "@SP\nAM=M-1\nD=M\n@$(5 + index)\nM=D\n"
     elseif segment == "pointer"
         ptr_label = index == 0 ? "THIS" : "THAT"
-        body = """
-        @SP
-        AM=M-1
-        D=M
-        @$ptr_label
-        M=D
-        """
+        body = "@SP\nAM=M-1\nD=M\n@$ptr_label\nM=D\n"
     elseif segment == "static"
-        body = """
-        @SP
-        AM=M-1
-        D=M
-        @$filename.$index
-        M=D
-        """
+        body = "@SP\nAM=M-1\nD=M\n@$filename.$index\nM=D\n"
     end
     return header * "\n" * body
 end
 
-# Main Logic
+# --- Program Flow Handlers (Project 08) ---
+
+function handleLabel(label_name, current_function)
+    # Labels are scoped to the function
+    return "($current_function\$$label_name)\n"
+end
+
+function handleGoto(label_name, current_function)
+    return "@$current_function\$$label_name\n0;JMP\n"
+end
+
+function handleIfGoto(label_name, current_function)
+    return """@SP
+AM=M-1
+D=M
+@$current_function\$$label_name
+D;JNE
+"""
+end
+
+# --- Function Calling Handlers (Project 08) ---
+
+function handleFunction(func_name, num_locals)
+    asm = "($func_name)\n"
+    for i in 1:num_locals
+        asm *= handlePush("constant", 0, "")
+    end
+    return asm
+end
+
+function handleCall(func_name, num_args, call_counter)
+    ret_addr = "RET_ADDR_$call_counter"
+    asm = "// call $func_name $num_args\n"
+    # push returnAddress
+    asm *= "@$ret_addr\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+    # push LCL, ARG, THIS, THAT
+    for seg in ["LCL", "ARG", "THIS", "THAT"]
+        asm *= "@$seg\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+    end
+    # ARG = SP - 5 - num_args
+    asm *= "@SP\nD=M\n@5\nD=D-A\n@$num_args\nD=D-A\n@ARG\nM=D\n"
+    # LCL = SP
+    asm *= "@SP\nD=M\n@LCL\nM=D\n"
+    # goto func_name
+    asm *= "@$func_name\n0;JMP\n"
+    # (returnAddress)
+    asm *= "($ret_addr)\n"
+    return asm
+end
+
+function handleReturn()
+    return """// return
+@LCL
+D=M
+@R13
+M=D
+@5
+A=D-A
+D=M
+@R14
+M=D
+@SP
+AM=M-1
+D=M
+@ARG
+A=M
+M=D
+@ARG
+D=M+1
+@SP
+M=D
+@R13
+AM=M-1
+D=M
+@THAT
+M=D
+@R13
+AM=M-1
+D=M
+@THIS
+M=D
+@R13
+AM=M-1
+D=M
+@ARG
+M=D
+@R13
+AM=M-1
+D=M
+@LCL
+M=D
+@R14
+A=M
+0;JMP
+"""
+end
+
+# --- Main Logic ---
 
 println("enter a path:")
 input_path = readline()
-# Clean input path from quotes and trailing slashes
 clean_path = strip(input_path, ['/', '\\', '\"', '\''])
 last_folder = basename(clean_path)
 
-# List all files and filter for .vm files
 all_files = readdir(clean_path)
 vm_files = filter(f -> endswith(f, ".vm"), all_files)
-
-# Define output .asm file path named after the directory
 output_path = joinpath(clean_path, last_folder * ".asm")
 
 open(output_path, "w") do outfile
     label_counter = 0
+    call_counter = 0
+    current_function = "Main.main" # Default context
+
+    # 1. Bootstrapping
+    if length(vm_files) > 1 || any(f -> f == "Sys.vm", vm_files)
+        println(outfile, "// --- Bootstrapping ---")
+        println(outfile, "@256\nD=A\n@SP\nM=D")
+        println(outfile, handleCall("Sys.init", 0, "BOOT"))
+    end
 
     for vmfile in vm_files
         current_file_base = splitext(vmfile)[1]
         full_path = joinpath(clean_path, vmfile)
-        
-        # Add a comment to differentiate between source files in the output
-        println(outfile, "// Translating file: $vmfile")
+        println(outfile, "// --- Translating file: $vmfile ---")
         
         open(full_path, "r") do f
             for line in eachline(f)
                 line = strip(line)
-                # Skip empty lines and comments
-                if isempty(line) || startswith(line, "//")
-                    continue
-                end
-
-                # Clean inline comments from the line
+                if isempty(line) || startswith(line, "//") continue end
+                
                 clean_line = split(line, "//")[1] |> strip
                 words = split(clean_line)
                 cmd = words[1]
 
-                # Arithmetic and Logical commands
-                if cmd == "add"
-                    println(outfile, handleAdd())
-                elseif cmd == "sub"
-                    println(outfile, handleSub())
-                elseif cmd == "neg"
-                    println(outfile, handleNeg())
-                elseif cmd == "and"
-                    println(outfile, handleAnd())
-                elseif cmd == "or"
-                    println(outfile, handleOr())
-                elseif cmd == "not"
-                    println(outfile, handleNot())
-                
-                # Comparison commands (require unique labels)
-                elseif cmd == "eq"
+                if cmd == "add" println(outfile, handleAdd())
+                elseif cmd == "sub" println(outfile, handleSub())
+                elseif cmd == "neg" println(outfile, handleNeg())
+                elseif cmd == "and" println(outfile, handleAnd())
+                elseif cmd == "or" println(outfile, handleOr())
+                elseif cmd == "not" println(outfile, handleNot())
+                elseif cmd == "eq" 
                     label_counter += 1
                     println(outfile, handleEq(label_counter))
-                elseif cmd == "gt"
+                elseif cmd == "gt" 
                     label_counter += 1
                     println(outfile, handleGt(label_counter))
-                elseif cmd == "lt"
+                elseif cmd == "lt" 
                     label_counter += 1
                     println(outfile, handleLt(label_counter))
-                
-                # Memory Access commands (push/pop)
                 elseif cmd == "push"
-                    segment = String(words[2])
-                    index = parse(Int, words[3])
-                    println(outfile, handlePush(segment, index, current_file_base))
+                    println(outfile, handlePush(String(words[2]), parse(Int, words[3]), current_file_base))
                 elseif cmd == "pop"
-                    segment = String(words[2])
-                    index = parse(Int, words[3])
-                    println(outfile, handlePop(segment, index, current_file_base))
+                    println(outfile, handlePop(String(words[2]), parse(Int, words[3]), current_file_base))
+                
+                # --- Project 08 Commands ---
+                elseif cmd == "label"
+                    println(outfile, handleLabel(words[2], current_function))
+                elseif cmd == "goto"
+                    println(outfile, handleGoto(words[2], current_function))
+                elseif cmd == "if-goto"
+                    println(outfile, handleIfGoto(words[2], current_function))
+                elseif cmd == "function"
+                    current_function = words[2]
+                    println(outfile, handleFunction(words[2], parse(Int, words[3])))
+                elseif cmd == "call"
+                    call_counter += 1
+                    println(outfile, handleCall(words[2], parse(Int, words[3]), call_counter))
+                elseif cmd == "return"
+                    println(outfile, handleReturn())
                 end
             end
         end
